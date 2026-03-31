9aa8f49acd48623f852695cd3798835688d5f174
Dung <buivandung11052005@gmail.com>
2026-03-31 22:36:57 +0700

    new update

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,8 +2,4 @@ from app.models.user import User
 from app.models.product import Product, Category
 from app.models.order import Order, OrderItem
 from app.models.payment import Payment, Promotion
-from app.models.customer import PointHistory
-from app.models.inventory import Warehouse, InventoryBatch, InventoryTransaction
-from app.models.production import Recipe, RecipeIngredient, ProductionPlan
-from app.models.ops import Shift, SystemLog
 from app.models.admin import AdminTodo
